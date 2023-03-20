3e4b6dcae24952cfd2af964d1708144d79677120
DebiSen1 <debi.sen@beas.co.in>
2023-03-20 16:39:20 +0530

    rating controller started

--- src/main/java/org/debisen/LibraryApplication/controller/RatingController.java
+++ src/main/java/org/debisen/LibraryApplication/controller/RatingController.java
@@ -1,5 +1,6 @@
 package org.debisen.LibraryApplication.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/ratings")
 public class RatingController {
 
+	@PostMapping(value = "/rate")
+	public void addrating() {
+		
+	}
 }
